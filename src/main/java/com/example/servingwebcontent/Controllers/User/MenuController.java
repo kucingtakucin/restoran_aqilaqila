9054511c574ff0709d0a62dc4b2d9ff34d128087
Anggita Sari Wulandari <anggitasw5@gmail.com>
2021-06-13 22:01:19 +0700

    bntar nyoba

--- src/main/java/com/example/servingwebcontent/Controllers/User/MenuController.java
+++ src/main/java/com/example/servingwebcontent/Controllers/User/MenuController.java
@@ -1,5 +1,13 @@
 package com.example.servingwebcontent.Controllers.User;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MenuController {
     
+    @GetMapping("/menu")
+	public String index(){
+		return "User/Menu/menu";	
+    }
 }
