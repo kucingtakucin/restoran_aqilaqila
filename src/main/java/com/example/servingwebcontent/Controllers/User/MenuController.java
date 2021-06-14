6248e56001e03bdc3c6343fc36fea71d94ac8163
labkomers <labkom@mipa.uns.ac.id>
2021-06-14 08:37:04 +0700

    Mengerjakan menu

--- src/main/java/com/example/servingwebcontent/Controllers/User/MenuController.java
+++ src/main/java/com/example/servingwebcontent/Controllers/User/MenuController.java
@@ -1,5 +1,76 @@
 package com.example.servingwebcontent.Controllers.User;
 
+import java.util.Optional;
+
+import com.example.servingwebcontent.Models.Menu;
+import com.example.servingwebcontent.Repositories.MenuRepository;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class MenuController {
     
+    @Autowired
+    private MenuRepository menuRepository;
+
+    @GetMapping("/menu")
+    public String index() {
+		return "User/Menu/menu";	
+    }
+
+    @GetMapping("/menu/data")
+    public @ResponseBody Iterable<Menu> data() {
+        // This returns a JSON or XML with the users
+        return menuRepository.findAll();
+    }
+
+    @PostMapping("/menu") // Map ONLY POST Requests
+    public @ResponseBody Menu insert (
+		@RequestBody @Validated Menu menu
+	) {
+      	// @ResponseBody means the returned String is the response, not a view name
+      	// @RequestParam means it is a parameter from the GET or POST request
+		return menuRepository.save(menu);
+    }
+
+	@GetMapping("/menu/{id}")
+	public @ResponseBody Optional<Menu> show (
+		@PathVariable("id") Integer id
+	) {
+		return menuRepository.findById(id);
+	}
+
+	@PutMapping("/menu/{id}")
+	public @ResponseBody ResponseEntity<Menu> update (
+		@PathVariable("id") Integer id,
+		@RequestBody @Validated Menu menu
+	) {
+		return menuRepository.findById(id)
+			.map(obj -> {
+				obj.setId(id);
+				return ResponseEntity.ok(menuRepository.save(menu));
+			}).orElseGet(() -> ResponseEntity.notFound().build());
+	}
+
+	@DeleteMapping("/menu/{id}")
+	public @ResponseBody ResponseEntity<Menu> delete (
+		@PathVariable("id") Integer id,
+		@RequestBody @Validated Menu menu
+	) {
+		return menuRepository.findById(id)
+			.map(obj -> {
+				menuRepository.deleteById(id);
+				return ResponseEntity.ok(obj);
+			}).orElseGet(() -> ResponseEntity.notFound().build());
+	}
 }
---
+++ src/main/java/com/example/servingwebcontent/Models/Menu.java
@@ -0,0 +1,57 @@
+package com.example.servingwebcontent.Models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class Menu {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+    private String nama;
+    private String deskripsi;
+    private String harga;
+    private String foto;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getDeskripsi() {
+        return deskripsi;
+    }
+
+    public void setDeskripsi(String deskripsi) {
+        this.deskripsi = deskripsi;
+    }
+
+    public String getHarga() {
+        return harga;
+    }
+
+    public void setHarga(String harga) {
+        this.harga = harga;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+}
